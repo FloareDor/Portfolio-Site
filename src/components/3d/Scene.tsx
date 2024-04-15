3a29e54376272d49444eb3fd0ec76977d02caa25
Ra <ravicity999@gmail.com>
2024-04-15 04:35:22 -0400

    feat: added a massive/bigger torus and made it interactable

--- src/components/3d/Scene.tsx
+++ src/components/3d/Scene.tsx
@@ -1,16 +1,19 @@
-'use-client';
+'use client';
+
 import React from 'react';
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber';
 import Model from './Model';
+import SmallModel from './smallModel';
 import { Environment } from '@react-three/drei';
 
+
 export default function Scene() {
-	return (
-		<Canvas style={{backgroundColor: "black"}}>
-			<directionalLight intensity={3} position={[0,3,2]}/>
-			<Environment preset='city'/>
-			<Model/>
-			
-		</Canvas>
-	)
+  return (
+    <Canvas style={{ backgroundColor: 'black' }}>
+      <directionalLight intensity={3} position={[0, 3, 2]} />
+      <Environment preset="city" />
+		  <SmallModel />
+		  <Model/>
+    </Canvas>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,23 @@ import { Canvas } from '@react-three/fiber';
 import { Text } from '@react-three/drei';
 import Box from '@/components/3d/box';
 import Navbar from '@/components/navbar/navbar';
-
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 
 const Scene = dynamic(() => import('@/components/3d/Scene'), {
-	ssr: false,
-	loading: () => <p>Loading...</p>
+  ssr: false,
+  loading: () => <p>Loading...</p>,
 });
 
 const Home: React.FC = () => {
   return (
-    <main className="h-screen w-screen flex flex-col">
-      <Navbar />
-      <Scene/>
+    <main className="h-screen w-screen flex flex-col relative">
+      <div className="absolute top-0 left-0 w-full h-full z-0">
+        <Scene/>
+      </div>
+      <div className="z-10">
+        <Navbar />
+      </div>
+      <div className="flex-grow"></div>
     </main>
   );
 };
