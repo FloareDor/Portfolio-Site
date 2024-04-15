f2da726f0db1396560c939b03371bd2db25d81c8
Ra <ravicity999@gmail.com>
2024-04-15 16:05:24 -0400

    chore: changed a default padding top value

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: React.FC = () => {
       <div className="z-10">
         <Navbar />
       </div>
-      <div className='text-white text-4xl text-center font-neue-montreal my-64'>
+      <div className='text-white text-4xl text-center font-neue-montreal pt-[43vh]'>
         Hi I&apos;m Ravi
       </div>
       <div className="flex-grow"></div>
