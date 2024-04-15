eacca085e372661ecde44fdcdedb4ab3311e7fb6
Ra <ravicity999@gmail.com>
2024-04-15 16:35:23 -0400

    feat: added a basic contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import Navbar from "@/components/navbar/navbar";
 const contact = () => {
 	return (
 		<div>
 			<div className="flex flex-col">
 				<Navbar />
-				<div className="flex flex-col items-center">
-					<span className="text-white">contact</span>
+				<div className="flex flex-col items-center gap-2 pt-28 lg:pt-60 md:pt-60 sm:pt-60">
+					<span className="text-white">gangavarapus@ufl.edu</span>
+					<Link href={"https://github.com/FloareDor"}><span className="text-white">GitHub</span></Link>
+					<Link href={"https://www.linkedin.com/in/sai-ravi-teja-gangavarapu/"}><span className="text-white">Linkedin</span></Link>
+					<Link href={"https://soundcloud.com/raven-714331711"}><span className="text-white">SoundCloud</span></Link>
+					<Link href={"https://www.instagram.com/floare_dor/"}><span className="text-white">Instagram</span></Link>
+					
+					
 				</div>
 			</div>
 		</div>
