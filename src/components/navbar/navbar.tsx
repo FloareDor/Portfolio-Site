6a26c0d69de91b5510e88bbf60b926bcabe2409c
Ra <ravicity999@gmail.com>
2024-04-16 14:14:46 -0400

    chore: small changes

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="bg-opacity-80 shadow-2xl shadow-black bg-black lg:bg-transparent md:bg-transparent sm:bg-transparent fixed top-0 left-0 right-0 z-20">
+    <div className="bg-opacity-80 shadow-sm shadow-black bg-black lg:bg-transparent md:bg-transparent sm:bg-transparent fixed top-0 left-0 right-0 z-20">
       <div className="flex flex-row items-center justify-between px-5 py-4 md:px-12 md:pt-10 lg:px-12 lg:pt-10">
         <span className="text-white text-3xl lg:text-3xl md:text-3xl sm:text-3xl font-bold">
           <Link href={"/"}>
@@ -21,7 +21,7 @@ const Navbar = () => {
         <div className="hidden lg:flex md:flex flex-row gap-5 items-center">
           <Link href={"/left-brain"}><span className="text-white text-sm lg:text-base font-semibold">Left Brain</span></Link>
           <Link href={"/right-brain"}><span className="text-white text-sm lg:text-base font-semibold">Right Brain</span></Link>
-          <Link href={"/about-me"}><span className="text-white text-sm lg:text-base font-semibold">About Me</span></Link>
+          <Link href={"/"}><span className="text-white text-sm lg:text-base font-semibold">About Me</span></Link>
           <Link href={"/contact"}><span className="text-white text-sm lg:text-base font-semibold">Contact</span></Link>
           <Link href={"/resume.pdf"}><span className="text-white text-sm lg:text-base font-semibold">Resume</span></Link>
         </div>
@@ -59,7 +59,7 @@ const Navbar = () => {
         <div className="gap-1 lg:hidden md:hidden px-5 py-4 flex flex-col items-end">
           <Link href={"/left-brain"}><span className="block text-white text-base font-semibold mb-2">Left Brain</span></Link>
           <Link href={"/right-brain"}><span className="block text-white text-base font-semibold mb-2">Right Brain</span></Link>
-          <Link href={"/about-me"}><span className="block text-white text-base font-semibold mb-2">About Me</span></Link>
+          <Link href={"/"}><span className="block text-white text-base font-semibold mb-2">About Me</span></Link>
           <Link href={"/contact"}><span className="block text-white text-base font-semibold mb-2">Contact</span></Link>
           <Link href={"/resume.pdf"}><span className="block text-white text-base font-semibold ">Resume</span></Link>
         </div>
