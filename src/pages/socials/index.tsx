7073f90516eab3f61adf5bfb58f16a4d7a0e4010
Ra <ravicity999@gmail.com>
2024-04-29 06:05:25 -0400

    chore: social link will now open a new tab

--- src/pages/socials/index.tsx
+++ src/pages/socials/index.tsx
@@ -1,21 +1,32 @@
 import Link from "next/link";
 import Navbar from "@/components/navbar/navbar";
+
 const socials = () => {
-	return (
-		<div>
-			<div className="flex flex-col bg-black">
-				<Navbar />
-				<div className="flex flex-col items-center gap-2 pt-28 lg:pt-60 md:pt-60 sm:pt-60">
-					<span className="text-white font-sans">gangavarapus@ufl.edu</span>
-					<Link href={"https://github.com/FloareDor"}><span className="text-white font-sans hover:text-gray-300">GitHub</span></Link>
-					<Link href={"https://www.linkedin.com/in/sai-ravi-teja-gangavarapu/"}><span className="text-white font-sans hover:text-gray-300">Linkedin</span></Link>
-					<Link href={"https://soundcloud.com/raven-714331711"}><span className="text-white font-sans hover:text-gray-300">SoundCloud</span></Link>
-					<Link href={"https://www.instagram.com/floare_dor/"}><span className="text-white font-sans hover:text-gray-300">Instagram</span></Link>
-					<Link href={"https://leetcode.com/Sai_Ravi_Teja/"}><span className="text-white font-sans hover:text-gray-300">Leetcode? Seriously?</span></Link>
-				</div>
-			</div>
-		</div>
-		
-	)
+    return (
+        <div>
+            <div className="flex flex-col bg-black">
+                <Navbar />
+                <div className="flex flex-col items-center gap-2 pt-28 lg:pt-60 md:pt-60 sm:pt-60">
+                    <span className="text-white font-sans">gangavarapus@ufl.edu</span>
+                    <Link href={"https://github.com/FloareDor"} target="_blank">
+                        <span className="text-white font-sans hover:text-gray-300">GitHub</span>
+                    </Link>
+                    <Link href={"https://www.linkedin.com/in/sai-ravi-teja-gangavarapu/"} target="_blank">
+                        <span className="text-white font-sans hover:text-gray-300">Linkedin</span>
+                    </Link>
+                    <Link href={"https://soundcloud.com/raven-714331711"} target="_blank">
+                        <span className="text-white font-sans hover:text-gray-300">SoundCloud</span>
+                    </Link>
+                    <Link href={"https://www.instagram.com/floare_dor/"} target="_blank">
+                        <span className="text-white font-sans hover:text-gray-300">Instagram</span>
+                    </Link>
+                    <Link href={"https://leetcode.com/Sai_Ravi_Teja/"} target="_blank">
+                        <span className="text-white font-sans hover:text-gray-300">Leetcode? Seriously?</span>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
 }
+
 export default socials;
